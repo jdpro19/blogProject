a325b84ec6eebb75c4c143d1f0d5f469b95cb0a5
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-03-18 12:26:27 +0530

    Added login logout functionality by modifying App.js and Login.js files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 import Home from "./pages/Home.js";
 import Login from "./pages/Login.js";
 import CreatePost from "./pages/CreatePost";
+import { useState } from 'react';
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
 
 function App() {
+
+  const [isAuth, setIsAuth] = useState(false);
+
+
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+     window.location.pathname = "/login";
+    });
+  }
+
   return (
     <Router>
     <nav>
       <Link to="/">Home</Link>
       <Link to="/createpost">CreatePost</Link>
-      <Link to="/login">Login</Link>
+      {!isAuth ? ( <Link to="/login">Login</Link> 
+      ) : (
+         <button onClick={signUserOut}> Sign Out</button> 
+      )}
     </nav>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/login" element={<Login/>}/>
+      <Route path="/login" element={<Login setIsAuth={setIsAuth}/>}/>
       <Route path="/createpost" element={<CreatePost/>}/>
     </Routes>
   </Router>
